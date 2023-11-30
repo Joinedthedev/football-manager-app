a0c3a49ffc24dc0edacb3fb31fa6e06cbf2c6bc3
Salim <salimbabaji@gmail.com>
2023-11-30 06:31:53 +0100

    First real commit

---
+++ football-manager-app/src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Sidebar from './components/Sidebar'
+import { Container } from 'react-bootstrap'
+
+function App() {
+
+
+  return (
+    <>
+    <Container>
+     <div> I am here</div> 
+    </Container>
+  
+    <Sidebar/>
+     </>
+    
+  )
+}
+
+export default App
---
+++ football-manager-app/src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div style={{height: '100vh', width: '60px', background: '#111', padding: '0px', margin: '0px' }}>
+     I AM SIDE BAR
+    </div>
+  );
+};
+
+export default Sidebar;
