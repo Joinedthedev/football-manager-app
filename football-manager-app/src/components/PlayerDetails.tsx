74e99c4f7070b47fea62869b179a698596737da4
Salim <salimbabaji@gmail.com>
2023-12-05 23:03:52 +0100

    Player component accurately maps values to screen

--- football-manager-app/src/components/PlayerDetails.tsx
+++ football-manager-app/src/components/PlayerDetails.tsx
@@ -19,26 +19,45 @@ type PlayerDetailsProps = {
 
 const PlayerDetails = ({ player }: PlayerDetailsProps) => {
   return (
-    <div className={styles.playerDetailsStyles}>
-      <div className={styles.playerInfo}>
-        <img
-          className={styles.flagImage}
-          src={player.flagImage}
-          alt="Flag Image"
-        />
-        <div className={styles.playerName}>{player.name}</div>
-      </div>
-      <div  style={{textAlign:"center", width:"100px"}}>{player.jerseyNumber}</div>
-      <div style={{textAlign:"center", width:"200px"}}>{player.starter}</div>
-      <div>{player.position}</div>
-      <div style={{width:"150px"}} >{(player.height / 100).toFixed(2)}m</div>
-      <div style={{width:"150px"}}>{player.weight}kg</div>
-      <div >{player.nationality}</div>
+    <>
+      <div className={styles.playerDetailsStyles}>
+        <div className={styles.playerInfo}>
+          <img
+            className={styles.flagImage}
+            src={player.flagImage}
+            alt="Flag Image"
+          />
+          <div style={{ width: "200px" }}>{player.name}</div>
+        </div>
+        <div style={{ textAlign: "center", width: "100px" }}>
+          {player.jerseyNumber}
+        </div>
+        <div style={{ textAlign: "center", width: "200px" }}>
+          {player.starter}
+        </div>
+        <div>{player.position}</div>
+        <div style={{ width: "150px" }}>
+          {(player.height / 100).toFixed(2)}m
+        </div>
+        <div style={{ width: "150px" }}>{player.weight}kg</div>
+        <div>{player.nationality}</div>
 
-      
-      <div>{player.appearances}</div>
-      <div>{player.minutesPlayed}</div>
-    </div>
+        <div style={{ textAlign: "center" }}>{player.appearances}</div>
+
+        <div style={{ textAlign: "center" }}>{player.minutesPlayed} </div>
+        <div
+        
+          style={{
+            width: "0px",
+            margin: "12px",
+            fontSize: "16px",
+            fontWeight: "900",
+          }}
+        >
+          ...
+        </div>
+      </div>
+    </>
   );
 };
 
