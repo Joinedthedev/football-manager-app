3cc2c95a5c00b5035b300dab69977ce89d9e36e9
Salim <salimbabaji@gmail.com>
2023-12-05 02:24:34 +0100

    Added error handling to ImportTeamModal

--- football-manager-app/src/components/ImportTeamModal.tsx
+++ football-manager-app/src/components/ImportTeamModal.tsx
@@ -4,7 +4,9 @@ import ReactDom from "react-dom";
 import ModalHeader from "./ModalHeader";
 import ModalLine from "../assets/component-assets/ModalLine";
 import Papa from "papaparse";
-import { useState, ChangeEvent } from "react";
+import { useState, useRef } from "react";
+import Button from "./SecondaryButton";
+import SecondaryButton from "./SecondaryButton";
 
 type ImportTeamModalProps = {
   open: boolean;
@@ -17,28 +19,66 @@ type Player = {
 };
 
 const ImportTeamModal = ({ open, onClose }: ImportTeamModalProps) => {
+  const defaultFormatMessage: string = "File must be in .csv format";
+  const wrongFormatMessage: string =
+    "Wrong file format. Please try again with a .csv file.";
+  const emptyValuesMessage: string =
+    "Your sheet is missing data. Please ensure all cells are filled out.";
+
+  //states for managing file importation
   const [importedData, setImportedData] = useState<Player[]>([]);
-  const [error, setError] = useState<string | null>(null); //error can either be null or a string and its initial state is null
+  const [error, setError] = useState<string>(defaultFormatMessage); //error can either be null or a string and its initial state is null
+  const [isError, setIsError] = useState<boolean>(false);
   const [fileName, setFileName] = useState<string>("");
-  
+
+  const hiddenFileInput = useRef<HTMLInputElement | null>(null);
+  const handleClick = () => {
+    hiddenFileInput.current.click();
+  };
+
   const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
+
+    setFileName(file.name);
 
     if (!file) {
       return;
     }
 
-    if (!file.name.endsWith(".csv")) {
-      setError("Invalid file format. Please select a .csv file.");
+    if (file.name.endsWith(".csv")) {
+      setError(defaultFormatMessage);
+      setIsError(false);
       return;
     }
+
+    if (!file.name.endsWith(".csv")) {
+      setError(wrongFormatMessage);
+      setIsError(true);
+      return;
+    }
+
+   
 
     if (file) {
       Papa.parse(file, {
         header: true,
         dynamicTyping: true,
         complete: function (result) {
-          console.log(result);
+          const parsedData = result.data as Player[];
+
+          const hasEmptyValues = parsedData.some((player) =>
+            Object.values(player).some(
+              (value) => value === null || value === undefined || value === ""
+            )
+          );
+
+          if (hasEmptyValues) {
+            setError(emptyValuesMessage);
+            setIsError(true)
+            return;
+          }
+
+          setImportedData(parsedData);
         },
       });
     }
@@ -51,23 +91,33 @@ const ImportTeamModal = ({ open, onClose }: ImportTeamModalProps) => {
           <div className={styles.modalStyles}>
             <ModalHeader title={"Importer"} onClose={onClose} />
             <ModalLine />
-     
-            <div className={styles.modalImportButtonContainer}> 
-<p className={styles.modalFileName}>Import text here</p>
-                <button className={styles.modalImportButton}>
-                      Select File  
-                      </button>
+
+            <div className={styles.modalImportRosterContainer}>
+              <p className={styles.modalRosterText}>Roster File</p>
+              <div
+                className={`${styles.modalImportButtonContainer} ${
+                  isError ? styles.modalImportRosterError : null
+                }`}
+              >
+                <p className={styles.modalFileName}>{fileName}</p>
+                <div onClick={handleClick}>
+                  <button
+                    className={`${styles.modalFileButton} ${
+                      isError ? styles.modalImportRosterError : null
+                    }`}
+                  >
+                    Select File
+                  </button>
+                </div>
+              </div>
+              <p className={styles.modalErrorText}>{error}</p>
             </div>
-                <input
-             
-                  className={styles.modalInput}
-                  type="file"
-                  onChange={handleFile}
-                  
-                />
-               
-          
-            <p className={styles.modalErrorText}></p>
+            <input
+              className={styles.modalInput}
+              type="file"
+              onChange={handleFile}
+              ref={hiddenFileInput}
+            />
           </div>
         </div>
       )}
