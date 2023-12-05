f89550ed18206bee65823a069f36162b07f9b40f
Salim <salimbabaji@gmail.com>
2023-12-05 15:05:20 +0100

    Added conditional rendering for import teams

--- football-manager-app/src/components/RosterSection.tsx
+++ football-manager-app/src/components/RosterSection.tsx
@@ -5,19 +5,22 @@ import PlayerStatLabels from "./PlayerStatLabels";
 import EditTeamName from "./EditTeamName";
 import ImportTeamModal from "./ImportTeamModal";
 import { useState } from "react";
+import PlayerRoster from "./PlayerRoster";
+import { usePlayerContext } from "./PlayerContext";
 
 /** The roster component for this app. ALL STYLES CAN BE FOUND IN src/styles/RosterSection.module.css */
 const RosterSection = () => {
+  const { players } = usePlayerContext();
   const [importTeamModalIsOpen, setImportTeamModalIsOpen] =
     useState<boolean>(false);
 
-    const handleOpenImportTeamModal = ()=>{
-        setImportTeamModalIsOpen(true)
-    }
+  const handleOpenImportTeamModal = () => {
+    setImportTeamModalIsOpen(true);
+  };
 
-    const handleCloseImportTeamModal = ()=>{
-        setImportTeamModalIsOpen(false)
-    }
+  const handleCloseImportTeamModal = () => {
+    setImportTeamModalIsOpen(false);
+  };
 
   return (
     <div className={styles.rosterContainer}>
@@ -29,27 +32,46 @@ const RosterSection = () => {
             className={styles.rosterInput}
             type="text"
           />
-          <button onClick = {handleOpenImportTeamModal}className={styles.rosterButton}> Import Team</button>
-        </div>
-      </div>
-      <div className={styles.rosterPlayerContainer}>
-        <PlayerStatLabels />
-        <div className={styles.rosterNoRosterContainer}>
-          {" "}
-          <p className={styles.rosterNoRosterText}>
-            You do not have any players on the roster
-          </p>{" "}
-          <p
+          <button
             onClick={handleOpenImportTeamModal}
-            className={styles.rosterImportTeamText}
+            className={styles.rosterButton}
           >
             {" "}
             Import Team
-          </p>
+          </button>
         </div>
       </div>
+      <div className={styles.rosterPlayerContainer}>
+        {/* {players.length > 0 ? (
+          <>
+            <PlayerRoster />
+          </>
+        ) : (
+          <>
+            <PlayerStatLabels />
+            <div className={styles.rosterNoRosterContainer}>
+              {" "}
+              <p className={styles.rosterNoRosterText}>
+                You do not have any players on the roster
+              </p>{" "}
+              <p
+                onClick={handleOpenImportTeamModal}
+                className={styles.rosterImportTeamText}
+              >
+                {" "}
+                Import Team
+              </p>
+            </div>
+          </>
+
+        )} */}
+      <PlayerRoster/>
+      </div>
       {importTeamModalIsOpen && (
-        <ImportTeamModal open={importTeamModalIsOpen} onClose={handleCloseImportTeamModal} />
+        <ImportTeamModal
+          open={importTeamModalIsOpen}
+          onClose={handleCloseImportTeamModal}
+        />
       )}
     </div>
   );
