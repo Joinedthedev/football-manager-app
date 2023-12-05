3a380ecd8632b0b95f35cfb90b164172b22ed659
Salim <salimbabaji@gmail.com>
2023-12-05 18:05:19 +0100

    -maps each player in roster onto screen

---
+++ football-manager-app/src/components/PlayerRoster.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { usePlayerContext } from "./PlayerContext";
+import styles from "@/styles/PlayerRoster.module.css";
+import PlayerDetails from "./PlayerDetails";
+const PlayerRoster = () => {
+  const { players, deletePlayer, editPlayer } = usePlayerContext();
+  return (
+    <div className={styles.playerRosterContainer}>
+
+<div className={styles.playerDirectContainer}>
+  
+        {players.map((player) => (
+          <PlayerDetails key={player.jerseyNumber} player={player} />
+        ))}
+  
+      </div>
+</div>
+  );
+};
+
+export default PlayerRoster;
