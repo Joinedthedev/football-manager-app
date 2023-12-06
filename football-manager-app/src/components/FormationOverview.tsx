12ecd79425df6eb5600a89b859570b5db04ec019
Salim <salimbabaji@gmail.com>
2023-12-07 00:48:43 +0100

    Uodates isfile to global state

--- football-manager-app/src/components/FormationOverview.tsx
+++ football-manager-app/src/components/FormationOverview.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+
 import EditTeamName from "./EditTeamName";
 import styles from "@/styles/FormationOverview.module.css";
+import Field from "@/assets/Field.png";
+import PlayerCard from "./PlayerCard";
+import { usePlayerContext } from "./PlayerContext";
+import FormationOverviewModal from "./FormationOverviewModal";
 const FormationOverview = () => {
+  const { isFile } = usePlayerContext();
   return (
     <div className={styles.formationContainer}>
       <div className={styles.formationTop}>
         <EditTeamName />
       </div>
-      <div className={styles.formationFieldContainer}></div>
+      <div className={styles.formationFieldContainer}>
+        {" "}
+        {isFile ? (
+          <>
+            <div className={styles.formationFieldImgContainer}>
+              <img className={styles.formationFieldImg} src={Field} alt="" />
+            </div>
+            <PlayerCard />
+          </>
+        ) : (
+          <FormationOverviewModal title="No player data found" text="Please importer your roster first." />
+        )}
+      </div>
     </div>
   );
 };
