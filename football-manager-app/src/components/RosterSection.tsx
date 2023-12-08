910df4944764c50dd676d4a00ab97706a80b48af
Salim <salimbabaji@gmail.com>
2023-12-08 09:40:07 +0100

    -updated search function to use search button

--- football-manager-app/src/components/RosterSection.tsx
+++ football-manager-app/src/components/RosterSection.tsx
@@ -1,4 +1,3 @@
-
 import styles from "@/styles/RosterSection.module.css";
 import PlayerStatLabels from "./PlayerStatLabels";
 import EditTeamName from "./EditTeamName";
@@ -11,55 +10,72 @@ import EditPlayerModal from "./EditPlayerModal";
 /** The roster component for this app. ALL STYLES CAN BE FOUND IN src/styles/RosterSection.module.css */
 const RosterSection = () => {
   const { players } = usePlayerContext();
-  const {isRosterImported} = usePlayerContext();
-  const {isEditPlayerModalIsOpen1, setIsEditPlayerModalIsOpen1} = usePlayerContext();
-  const {setSearch} = usePlayerContext();
-
+  const { isRosterImported } = usePlayerContext();
+  const { isEditPlayerModalIsOpen1, setIsEditPlayerModalIsOpen1 } =
+    usePlayerContext();
+  const { setSearch } = usePlayerContext();
 
   const [importTeamModalIsOpen, setImportTeamModalIsOpen] =
     useState<boolean>(false);
-   
+
+  const [trackInput, setTrackInput] = useState<string>("");
 
   const handleOpenImportTeamModal = () => {
     setImportTeamModalIsOpen(true);
-   
   };
 
   const handleCloseImportTeamModal = () => {
     setImportTeamModalIsOpen(false);
-    
   };
 
+  const handleCloseEditPlayerModal = () => {
+    setIsEditPlayerModalIsOpen1(false);
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key == "Enter") {
+      setSearch(e.currentTarget.value);
+    }
 
-  const handleCloseEditPlayerModal =()=>{
-    setIsEditPlayerModalIsOpen1(false)
-  }
-  const handleChange = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key=='Enter'){
-    setSearch(e.currentTarget.value);
+    if (e.key== "Escape"){
+      setTrackInput("");
     }
   };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setTrackInput(value);
+  };
+
+  const handleClick = () =>{
+
+    setSearch(trackInput);
+  }
 
   return (
     <div className={styles.rosterContainer}>
       <div className={styles.rosterTop}>
         <EditTeamName />
         <div className={styles.rosterTopRight}>
-          <input
-            placeholder={" Find Player"}
-            className={styles.rosterInput}
-            type="text"
-         
-            onKeyDown={handleChange}
-         
-          />
-          {players.length > 0 && isRosterImported? (
+          {/* <form onSubmit={handleSubmit}> */}
+            <input
+              placeholder={" Find Player"}
+              className={styles.rosterInput}
+              type="text"
+              value={trackInput}
+              onKeyDown={handleKeyDown}
+              onChange={handleChange}
+            />
+            {trackInput.length > 0 ? (
+              <button type="submit" onClick={handleClick} className={styles.rosterSearchButton}> Search</button>
+            ) : null}
+          {/* </form> */}
+          {players.length > 0 && isRosterImported ? (
             <button
               onClick={handleOpenImportTeamModal}
               className={styles.rosterButtonReImport}
             >
               {" "}
-             Re-Import Team
+              Re-Import Team
             </button>
           ) : (
             <button
@@ -74,7 +90,7 @@ const RosterSection = () => {
       </div>
       <div className={styles.rosterPlayerContainer}>
         <PlayerStatLabels />
-        {players.length > 0 && isRosterImported? (
+        {players.length > 0 && isRosterImported ? (
           <>
             <PlayerRoster />
           </>
@@ -95,9 +111,7 @@ const RosterSection = () => {
               </p>
             </div>
           </>
-
         )}
-       
       </div>
       {importTeamModalIsOpen && (
         <ImportTeamModal
@@ -105,7 +119,10 @@ const RosterSection = () => {
           onClose={handleCloseImportTeamModal}
         />
       )}
-     <EditPlayerModal open={isEditPlayerModalIsOpen1} onClose={handleCloseEditPlayerModal}/>
+      <EditPlayerModal
+        open={isEditPlayerModalIsOpen1}
+        onClose={handleCloseEditPlayerModal}
+      />
     </div>
   );
 };
